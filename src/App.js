28657d5958b05b2cdd4f54b5f8ab718ba04d60f8
Caio <caiovinius@hotmail.com>
2022-05-22 17:22:43 -0300

    feat: add gantt chart and start api connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Gantt } from 'gantt-task-react';
+import "gantt-task-react/dist/index.css";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [processList, setProcessList] = useState();
+  const [cicle, setCicle] = useState(1);
+  // const [cicleExists, setCicleExists] = useState(true);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/cicle/get/${cicle}`)
+    .then(res => res.json())
+    .then(data => setProcessList(data));
+  }, [cicle]);
+
+  console.log(processList);
+
+  // do {
+  //   cicleCount++;
+  //   setCicle(cicleCount);
+  // }while(cicleExists);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {processList ? <Gantt tasks={
+        [
+          {
+            start: new Date(2020, 1, processList.process.A.arrival_time),
+            end: new Date(2020, 1, processList.process.A.arrival_time + processList.process.A.execution_time),
+            name: 'Processo ' + processList.process.A.name,
+            id: 'Task 0',
+            type:'task',
+            progress: 100,
+            isDisabled: true,
+            styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
+          },
+        ]
+      } /> : <p>Não há processos em execução.</p>}
+      
     </div>
   );
 }
