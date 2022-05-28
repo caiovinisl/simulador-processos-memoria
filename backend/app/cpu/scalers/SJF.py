9e80fd47926558df1a33aa6a9f584e56ca63e3e3
lucas <jlucas.ldm@gmail.com>
2022-05-28 18:11:56 -0300

    RR em desenvolvimento

---
+++ backend/app/cpu/scalers/SJF.py
@@ -0,0 +1,18 @@
+import os, sys
+
+# PARENT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# result = os.path.split(PARENT)[0]
+# sys.path.append(result)
+from cpu.models.process import ProcessIn
+from collections import deque
+
+
+
+def sjf(process_list:list[ProcessIn],time_count:int=None)-> deque[ProcessIn]:
+    d = deque()
+    print('fazendo sjf')
+    process_list.sort(key=lambda x: x.execution_time - x.already_exec, reverse=True)
+    for x in process_list:
+        d.append(x)
+    return d
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -90,4 +90,4 @@ def delete_all_cicle():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8030, reload=True)
